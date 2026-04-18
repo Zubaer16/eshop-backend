b8669fb52c03db0699da1048a373e8f5f2bf1310
Zubaer16 <zubaer.ahmed7690@gmail.com>
2026-04-18 23:34:12 +0600

    refactor: move configuration files alongside src and update import paths

--- src/infrastructure/auth/passport.config.ts
+++ src/infrastructure/auth/passport.config.ts
@@ -1,7 +1,7 @@
 import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
 import { Strategy as FacebookStrategy } from 'passport-facebook';
 import passport from 'passport';
-import { envConfig } from '../../config/env';
+import { envConfig } from '../../../config/env';
 import { AuthProvider } from '@prisma/client';
 import { OAuthService } from '../../modules/auth/oauth.service';
 
